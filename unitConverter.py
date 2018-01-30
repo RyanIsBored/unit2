6668d5add13fb87d5a850b23d03ad1fa7f6609cc
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-01-30 08:41:54 -0500

    Updated from Brython Server: 1/30/2018 8:41:53 AM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,16 @@
+#Ryan Jones
+#1/30/18
+#unitConverter.py
 
+print('1) Kilometers to Miles')
+print('2) Kilograms to Pounds')
+print('3) Liters to Gallons')
+print('4) Celsius to Fahrenheit')
+
+choice = int(input('Choose a number: '))
+
+
+
+
+if choice == 4:
+    celsius = float(input('Enter Degrees in Celsius: '))
