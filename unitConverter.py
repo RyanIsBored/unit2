7d952a114c42cf992428c9e16c0e3d47ecbf96b8
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-05 08:34:21 -0500

    Updated from Brython Server: 2/5/2018 8:34:20 AM

--- unitConverter.py
+++ unitConverter.py
@@ -12,22 +12,22 @@ choice = int(input('Choose a number: '))
 if choice == 1:
     kilometers = float(input('Enter number of Kilometers: '))
     miles = kilometers*0.621371
-    print(miles, 'miles')
+    print(kilometers,'Kilometers is',miles,'Miles')
 
 if choice == 2:
     kilograms = float(input('Enter number of Kilograms: '))
     pounds = kilograms*2.20462
-    print(pounds, 'pounds')
+    print(kilograms,'Kilograms is',pounds,'Pounds')
     
 if choice == 3:
     liters = float(input('Enter number of Liters: '))
     gallons = liters*0.264172
-    print(gallons, 'gallons')
+    print(liters,'Liters is',gallons,'Gallons')
 
 if choice == 4:
     celsius = float(input('Enter Degrees in Celsius: '))
     fahrenheit = celsius*1.8+32
-    print(fahrenheit, 'degrees Fahrenheit')
+    print(celsius,'degrees Celsius is',fahrenheit,'degrees Fahrenheit')
 
 if choice<1 or choice>4:
     print('No.')
