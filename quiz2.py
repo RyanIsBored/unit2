fcf555298c33427742ade7384100d5dd44fddc2e
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-12 08:30:39 -0500

    Updated from Brython Server: 2/12/2018 8:30:38 AM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,16 @@
+#Ryan Jones
+#2/12/18
+#quiz2.py
 
+bothwords = input('Enter two words: ')
+firstword, lastword = bothwords.split()
+
+if (len(firstword))>(len(lastword)):
+    print('The first word is longer')
+if (len(firstword))<(len(lastword)):
+    print('The second word is longer')
+if (len(firstword))==(len(lastword)):
+    print('The words are the same length')
+
+if 'p' in firstword and 'p' in lastword:
+    print('Both words have a p')
