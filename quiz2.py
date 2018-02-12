e21642dd838e678090cd871ab448313a7ece494a
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-02-12 08:37:40 -0500

    Updated from Brython Server: 2/12/2018 8:37:39 AM

--- quiz2.py
+++ quiz2.py
@@ -18,3 +18,10 @@ if 'p' not in firstword and 'p' in lastword:
     print('Only the second word has a p')
 if 'p' in firstword and 'p' not in lastword:
     print('Only the first word has a p')
+
+num1 = int(input('Enter two numbers that add up to 12 (first number): '))
+num2 = int(input('Enter the second number that adds up to 12: '))
+if num1+num2==12:
+    print('Correct')
+else:
+    print('Incorrect')
